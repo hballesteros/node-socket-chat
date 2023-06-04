ab16651e61e8ea1eb878bfdee7a10d6fca615c96
Hugo <hugo@boahstudio.com>
2023-06-04 19:38:15 -0300

    chat and private messages

--- sockets/controller.js
+++ sockets/controller.js
@@ -1,15 +1,41 @@
-const { Socket } = require("socket.io")
-const { comprobarJWT } = require("../helpers")
+const { Socket } = require('socket.io')
+const { comprobarJWT } = require('../helpers')
+const { ChatMensajes } = require('../models')
 
+const chatMensajes = new ChatMensajes()
 
-const socketController = async( socket = new Socket() ) => {
+const socketController = async( socket = new Socket(), io ) => {
 
     const usuario = await comprobarJWT(socket.handshake.headers['x-token'])
     if ( !usuario ){
         return socket.disconnect()
     }
 
-    console.log('Se conecto', usuario.nombre )
+    // Agregar el usuario conectado
+    chatMensajes.conectarUsuario( usuario )
+    io.emit('usuarios-activos', chatMensajes.usuariosArr )
+    socket.emit('recibir-mensajes', chatMensajes.ultimmos10 )
+
+    // Conectado a una sala especial
+    socket.join( usuario.id ) // salas: global, socket.id y usuario.id
+
+
+    // Limpiar cuando se desconecta
+    socket.on('disconnect', () => {
+        chatMensajes.desconectarUsuario( usuario.id )
+        io.emit('usuarios-activos', chatMensajes.usuariosArr )
+    })
+
+    socket.on('enviar-mensaje', ({ uid, mensaje }) => {
+        if ( uid ) {
+            //Mensaje privado
+            socket.to( uid ).emit('mensaje-privado', { de: usuario.nombre, mensaje })
+
+        } else {
+            chatMensajes.enviarMensaje(usuario.id, usuario.nombre, mensaje)
+            io.emit('recibir-mensajes', chatMensajes.ultimmos10 )
+        }
+    })
     
 }
 
